5d137aa6e2b06c2adea734664fd019c2da1754fd
Yaojie Li <jeffrey.yaojie.li@gmail.com>
2023-10-01 22:37:39 -0400

    Tutorial UI

--- StudyVR-Unity/Assets/Scripts/GameManagers/TutorialManager.cs
+++ StudyVR-Unity/Assets/Scripts/GameManagers/TutorialManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 LeftAnchorPosition;
+    private Vector3 RightAnchorPosition;
+    private Quaternion LeftAnchorRotation;
+    private Quaternion RightAnchorRotation;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Transform LeftHandTrans;
+    [SerializeField] private Transform RightHandTrans;
+    [SerializeField] private Transform CentreEyeTrans;
+
+    [SerializeField] private Transform LeftHandUI;
+    [SerializeField] private Transform RightHandUI;
+
+    private float FollowSpeed = 5.0f;
+
+    private void Update()
     {
-        
+        Vector3 LeftPosAnchor = new Vector3(LeftHandTrans.position.x - CentreEyeTrans.position.x, 0f, LeftHandTrans.position.z - CentreEyeTrans.position.z);
+        LeftAnchorPosition = LeftHandTrans.position + LeftPosAnchor.normalized * 0.2f + new Vector3(0f, 0.1f, 0f);
+
+        Vector3 RightPosAnchor = new Vector3(RightHandTrans.position.x - CentreEyeTrans.position.x, 0f, RightHandTrans.position.z - CentreEyeTrans.position.z);
+        RightAnchorPosition = RightHandTrans.position + RightPosAnchor.normalized * 0.2f + new Vector3(0f, 0.1f, 0f);
+
+        LeftAnchorRotation = LeftHandTrans.rotation * Quaternion.Euler(30f, 0f, 0f);
+        RightAnchorRotation = RightHandTrans.rotation * Quaternion.Euler(30f, 0f, 0f);
+
+        LeftHandUI.position = Vector3.Lerp(LeftHandUI.position, LeftAnchorPosition, Time.deltaTime * FollowSpeed);
+        RightHandUI.position = Vector3.Lerp(RightHandUI.position, RightAnchorPosition, Time.deltaTime * FollowSpeed);
+
+        LeftHandUI.rotation = Quaternion.Lerp(LeftHandUI.rotation, LeftAnchorRotation, Time.deltaTime * FollowSpeed);
+        RightHandUI.rotation = Quaternion.Lerp(RightHandUI.rotation, RightAnchorRotation, Time.deltaTime * FollowSpeed);
+
+
     }
 }
